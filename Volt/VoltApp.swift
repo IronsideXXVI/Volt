f7d81dead670768f65d8296697c9b39e152260a6
IronsideXXVI <Ironside.Dylan26@gmail.com>
2026-07-17 12:31:07 -0500

    Initial Commit

---
+++ Volt/VoltApp.swift
@@ -0,0 +1,32 @@
+//
+//  VoltApp.swift
+//  Volt
+//
+//  Created by Dylan Ironside on 7/17/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VoltApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
